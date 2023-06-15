1587e837f2e7c73e04eacd8509ae5a86e1021d63
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-06-15 15:16:03 +0500

    Add files via upload

---
+++ Variable+Data Type.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//	this is single comment
+/*
+this
+is 
+multi
+line
+comment
+*/	
+
+//variables-> container to store data
+//how to declare a variable
+int var1;
+//how to initialize variable
+var1 = 5;
+//declaration + initialization
+int var2 = 80;	
+
+//data types
+/*
+int -> wwhole nos e.g: 120 or -88
+float -> small decimal nos e.g: 64.34 
+double -> long decimal nos e.g: 164.34344 
+char -> single char e.g: 'c'
+string -> message e.g: "This is my message"
+bool -> true false 
+void -> empty
+*/
+	
+int var1 =4;
+float var2 = 64.56;
+double var3 = 134.5664;
+char var4 = 'f'	;
+string var5 = "Farzeen Ali";
+bool var6 = true;
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
