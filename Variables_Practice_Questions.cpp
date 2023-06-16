4315b27524743ac642ec826dbb754559699d7377
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-06-16 16:04:00 +0500

    Add files via upload

---
+++ Variables_Practice_Questions.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//	int value1 = 5;
+//	int value2 = 4;
+//	cout<<value2;
+	
+//	float value1 = 23.4;
+//	cout<<value1;
+	
+//	double value1 = 145.345;
+//	cout<<value1;
+	
+//	char char1 = 'a';
+//	cout<<char1;
+
+//string myName = "Farzeen";
+//cout<<myName;
+	
+//	bool condition = true;
+//	cout<<condition;
+	
+
+}
