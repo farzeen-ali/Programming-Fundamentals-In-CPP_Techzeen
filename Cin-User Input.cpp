663b51d904715cbd1cc6816f307d4d9d8bff41f2
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-06-17 16:32:51 +0500

    Add files via upload

---
+++ Cin-User Input.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	//int a, b;
+	string a,b;
+	cout<<"Enter The Values Of a and b: "<<endl;
+	//cin ---> user input
+	cin>>a>>b;
+	cout<<"The Value Of a is: "<<a<<endl<<"The Value of b is: "<<b;
+	
+	return 0;
+}
---
+++ Practice_Cin_User_input.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	//calculate 4nos average
+//	float value1, value2, value3, value4, avg;
+//	cout<<"Enter Four Values: "<<endl;
+//	cin>>value1>>value2>>value3>>value4;
+//	avg = (value1 + value2 + value3 + value4) / 4;
+//	cout<<"The Average of "<<value1<<","<<value2<<","<<value3<<" and "<<value4<<" : "<<avg<<endl;
+//	
+
+	//string concatenation
+	string firstName, lastName;
+	cout<<"Enter Your First and Last Name: "<<endl;
+	cin>>firstName>>lastName;
+	cout<<"Your Full Name Is: "<<firstName<<" "<<lastName;
+
+	return 0;
+}
