6d694fb2504010533006ab0de1f434dce983da09
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-06-19 16:21:54 +0500

    Add files via upload

---
+++ Operators and Its Types.cpp
@@ -0,0 +1,21 @@
+/*
+Operands: data on which operations are performed 
+Operators: types of operations performed on operands
+Expression: combination of operators and operands
+e.g:
+a+b
+Types Of Operators:
+1)Unary Operators (works on only 1 operand)
+2)Binary Operators (works on 2 operands)
+3)Ternary Operators (works on 3 operands)
+
+Categories:
+1)Arithmetic
+a)Unary(++,--) b)Binary(+,-,*,/,%)
+2)Relational(<,>,<=,>=,!=,==)
+3)Logical( && ,!,||)
+4)Assignment(=) 
+5)Special 
+a)Conditional(?(true):(false)) b)C++ Shorthands (+=,-=,*=,/=,%=) -> a= a+b -> a+=b
+*/
+
