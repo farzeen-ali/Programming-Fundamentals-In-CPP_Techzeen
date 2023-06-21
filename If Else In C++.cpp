853bf37087cda6e19281a82a4127ff9f75cc2329
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-06-21 22:23:35 +0500

    Add files via upload

---
+++ If Else In C++.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age;
+	cout<<"Enter your Age: "<<endl;
+	cin>>age;
+	//if else -> conditional statement
+//	if(condition)
+//	{
+//		//code
+//	}
+//	else
+//	{
+//		//code
+//	}
+
+	if(age>18)
+	{
+		cout<<"Your age is greater Than 18";
+	}
+	else
+	{
+		cout<<"You are under 18";
+	}
+	
+	return 0;
+}
